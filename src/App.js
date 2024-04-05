9754ea178d7f8896333a3846848f2bb584c9edec
Tyler Farrior <tylerfarrior@gmail.com>
2024-04-05 14:52:11 -0400

    adding to index.js file

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+// App.js
 
-function App() {
+import React from 'react';
+import './App.css'; // Import global styles
+import CarCatalog from './components/CarCatalog/CarCatalog'; // Import CarCatalog component
+
+const App = () => {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Welcome to My Car Catalog</h1>
       </header>
+      <main>
+        <CarCatalog />
+      </main>
     </div>
   );
-}
+};
 
 export default App;
