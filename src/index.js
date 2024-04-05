57e5b591e5c39741820de2baa9dbdafe056dceaa
Tyler Farrior <tylerfarrior@gmail.com>
2024-04-05 14:56:57 -0400

    adding tailwindcss

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css'; // You can import your global styles here
-import CarCatalog from './components/CarCatalog/CarCatalog'; // Assuming CarCatalog.js is your main component
-
+import './tailwind.css';
+import CarCatalog from './CarCatalog';
 ReactDOM.render(
   <React.StrictMode>
     <CarCatalog />
