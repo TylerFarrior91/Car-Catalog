d38695fc885a799a4104e9a762aeb2c8d657678e
Tyler Farrior <tylerfarrior@gmail.com>
2024-04-11 16:18:27 -0400

    removing css

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './tailwind.css';
+import './index.css';
 import CarCatalog from './CarCatalog';
 ReactDOM.render(
   <React.StrictMode>
